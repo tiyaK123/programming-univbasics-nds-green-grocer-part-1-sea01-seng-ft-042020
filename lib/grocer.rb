cbd4f9ee49ce1de4374d158371336ccbd38a107a
tiya adugna <tiyakefalee@gmail.com>
2020-04-15 04:17:32 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,8 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   index = 0
 while collection.length > index 
-  index.each do |item, price, clearance|
-    if item == name
+ 
+    if collection[item] == name
       hash = index
       return hash 
     end
